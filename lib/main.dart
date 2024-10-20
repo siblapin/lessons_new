6a59246fcd1e0d22cdc33d506e11d7760d124420
siblapin win <sib.lapin@mail.ru>
2024-10-20 21:26:37 +0700

    мук 00.2

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class Alpha16 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'TEST',
         theme: ThemeData(fontFamily: 'gilroy-bold'),
         debugShowCheckedModeBanner: false,
         home: Scaffold(
